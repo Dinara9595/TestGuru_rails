86b902d55e10cdee3fbd33f726ffe8d73f385374
Dinara9595 <dinara.petuhova95@yandex.ru>
2021-12-16 18:48:21 +0300

    Task_3

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,11 @@
 class Test < ApplicationRecord
+  class << self
+    def self.tests(category)
+      joins('JOIN categories ON tests.category_id == categories.id')
+      .where(categories: {name: category})
+      .select(:id, :title)
+      .order('title DESC')
+      .pluck('title')
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  def tests(level)
+    Test.joins('JOIN passed_tests ON tests.id == test_id')
+        .where(passed_tests: {user_id: self.id}, tests: {level: level})
+  end
 end
