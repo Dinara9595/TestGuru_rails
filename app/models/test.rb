614ab14998e145fc09021bf2ed9643902c1c7c96
Dinara9595 <dinara.petuhova95@yandex.ru>
2021-12-17 14:37:49 +0300

    Apply comments by review

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,8 +2,7 @@ class Test < ApplicationRecord
   def self.title_tests(name_category)
     joins('JOIN categories ON tests.category_id == categories.id')
     .where(categories: {name: name_category})
-    .select(:id, :title)
-    .order('title DESC')
+    .order('title: :DESC')
     .pluck('title')
   end
 end
