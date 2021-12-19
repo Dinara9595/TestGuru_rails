035e15d43e66eec86adc70d4884dc6f267745280
Dinara9595 <dinara.petuhova95@yandex.ru>
2021-12-19 23:33:51 +0300

    Task_4

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,10 @@
 class Test < ApplicationRecord
+  has_many :questions
+  belongs_to :category
+  has_many :tests_users
+  has_many :users, through: :tests_users
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
   def self.title_tests(name_category)
     joins('JOIN categories ON tests.category_id == categories.id')
     .where(categories: {name: name_category})
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-  def tests(level)
+  has_many :tests_users
+  has_many :tests, through: :tests_users
+  has_many :tests, inverse_of: 'author'
+
+  def level_tests(level)
     Test.joins('JOIN passed_tests ON tests.id == test_id')
         .where(passed_tests: {user_id: self.id}, tests: {level: level})
   end
