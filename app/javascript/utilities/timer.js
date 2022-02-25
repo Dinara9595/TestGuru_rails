3d8fcf8422ae09f15523a86e7fed874f953c587a
Dinara9595 <dinara.petuhova95@yandex.ru>
2022-02-25 14:53:36 +0300

    Apply comments by review. Finish_task_2

--- app/javascript/utilities/timer.js
+++ app/javascript/utilities/timer.js
@@ -1,6 +1,7 @@
 document.addEventListener('turbolinks:load', function() {
     let timeMinut = document.getElementById("minutes").dataset.leftTime;
     let timerShow = document.getElementById("timer");
+    let form = document.forms[0]
 
     timer = setInterval(function () {
         seconds = Math.floor(timeMinut) % 60
@@ -8,6 +9,7 @@ document.addEventListener('turbolinks:load', function() {
         if (timeMinut <= 0) {
             clearInterval(timer);
             alert("Время закончилось");
+            form.submit()
         } else {
             let strTimer = `${Math.trunc(minutes)}:${seconds}`;
             timerShow.innerHTML = strTimer;
